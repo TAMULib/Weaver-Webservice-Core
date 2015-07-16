e0532ed048bf2a83fc5ab4ce2f9ac5e55d9b82bf
jeremythuff <jeremythuff@gmail.com>
2015-07-16 16:07:51 -0500

    updated to release

--- framework/installme.sh
+++ framework/installme.sh
@@ -1,2 +1,2 @@
 mvn clean install
-mvn install:install-file -Dfile=core/target/webservice-framework-core-1.0-SNAPSHOT.jar -DgroupId=edu.tamu -DartifactId=webservice-framework -Dversion=1.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=core/target/webservice-framework-core-1.0-RELEASE.jar -DgroupId=edu.tamu.framework -DartifactId=webservice-framework -Dversion=1.0-RELEASE -Dpackaging=jar
