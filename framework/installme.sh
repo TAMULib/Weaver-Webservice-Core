a479a904122f0e32a0398abfc32881884c69c538
jeremythuff <jeremythuff@gmail.com>
2015-07-17 15:43:14 -0500

    Updated the paths and ids of the install script

--- framework/installme.sh
+++ framework/installme.sh
@@ -1,2 +1,2 @@
 mvn clean install
-mvn install:install-file -Dfile=core/target/webservice-framework-core-1.0-RELEASE.jar -DgroupId=edu.tamu.framework -DartifactId=webservice-framework -Dversion=1.0-RELEASE -Dpackaging=jar
+mvn install:install-file -Dfile=core/target/webservice-framework-core-1.0.jar -DgroupId=edu.tamu.framework -DartifactId=webservice-framework-core -Dversion=1.0 -Dpackaging=jar
