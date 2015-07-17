07ac69bf168d0b91055073ce7b2f8f1074f8aa3c
jeremythuff <jeremythuff@gmail.com>
2015-07-17 13:54:10 -0500

    Renamed controller aspect to have 'core' in the name

--- seed-app/src/main/java/edu/tamu/app/aspect/AppControllerAspect.java
+++ seed-app/src/main/java/edu/tamu/app/aspect/AppControllerAspect.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import edu.tamu.app.model.repo.UserRepo;
-import edu.tamu.framework.aspect.ControllerAspect;
+import edu.tamu.framework.aspect.CoreControllerAspect;
 
 
 @Component
 @Aspect
-public class AppControllerAspect extends ControllerAspect {
+public class AppControllerAspect extends CoreControllerAspect {
 
 	@Autowired
 	UserRepo userRepo;
