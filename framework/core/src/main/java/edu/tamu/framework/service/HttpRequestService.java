ce6e98a7dce2cc4d9b71349e2d19c7af3db20add
William Welling <wwelling110381@outlook.com>
2015-08-06 09:39:08 -0500

    Added abstract method for http request service.

--- framework/core/src/main/java/edu/tamu/framework/service/HttpRequestService.java
+++ framework/core/src/main/java/edu/tamu/framework/service/HttpRequestService.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Service;
 import edu.tamu.framework.model.HttpRequest;
 
 @Service
-public class HttpRequestService {
+public abstract class HttpRequestService {
 	
-	private List<HttpRequest> requests = new ArrayList<HttpRequest>();
+	protected List<HttpRequest> requests = new ArrayList<HttpRequest>();
 
 	public List<HttpRequest> getRequests() {
 		return requests;
@@ -31,16 +31,27 @@ public class HttpRequestService {
 	}
 	
 	public synchronized HttpServletRequest getAndRemoveRequestByDestinationAndUser(String destination, String user) {
+		
 		HttpServletRequest httpServletRequest = null;
+		
 		for(int index = 0; index < requests.size(); index++) {
+		
 			HttpRequest request = requests.get(index);
+			
 			if(request.getUser().equals(user) && request.getDestination().contains(destination)) {
 				httpServletRequest = request.getRequest();
 				requests.remove(index);
 				break;
 			}
+			
+			if(httpServletRequest == null)
+				httpServletRequest = getRequestAndSetRequest(destination, user, index);
+			
 		}
+		
 		return httpServletRequest;
 	}
+	
+	public abstract HttpServletRequest getRequestAndSetRequest(String destination, String user, int index);
 
 }
