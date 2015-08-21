e9485d30e1c5c64d1dde679b9eac110f57c81f02
William Welling <wwelling110381@outlook.com>
2015-08-21 16:21:57 -0500

    Put Annotator role in order on enum.

--- framework/core/src/main/java/edu/tamu/framework/enums/CoreRoles.java
+++ framework/core/src/main/java/edu/tamu/framework/enums/CoreRoles.java
@@ -1,9 +1,9 @@
 package edu.tamu.framework.enums;
 
-public enum CoreRoles {
-	ROLE_ANNOTATOR,
+public enum CoreRoles {	
 	ROLE_ANONYMOUS, 
 	ROLE_USER, 
+	ROLE_ANNOTATOR,
 	ROLE_MANAGER, 
 	ROLE_ADMIN
 }
