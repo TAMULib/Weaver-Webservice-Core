d45df85b54492abd36a5123d4a8c96ec73e93812
William Welling <wwelling110381@outlook.com>
2015-09-14 11:17:34 -0500

    Removed confir user endpoint. Handled in framework.

--- seed-app/src/main/java/edu/tamu/app/controller/AdminController.java
+++ seed-app/src/main/java/edu/tamu/app/controller/AdminController.java
@@ -9,25 +9,8 @@
  */
 package edu.tamu.app.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import edu.tamu.app.model.AppUser;
-import edu.tamu.app.model.repo.UserRepo;
-import edu.tamu.framework.aspect.annotation.Auth;
-import edu.tamu.framework.aspect.annotation.ReqId;
-import edu.tamu.framework.aspect.annotation.Shib;
-import edu.tamu.framework.model.ApiResponse;
-import edu.tamu.framework.model.Credentials;
-import edu.tamu.framework.model.RequestId;
 
 /** 
  * Admin Controller.
@@ -38,48 +21,5 @@ import edu.tamu.framework.model.RequestId;
 @RestController
 @MessageMapping("/admin")
 public class AdminController {
-	
-	@Autowired
-	public ObjectMapper objectMapper;
-	
-	@Autowired
-	private UserRepo userRepo;
-	
-	@Autowired 
-	private SimpMessagingTemplate simpMessagingTemplate; 
-	
-	@MessageMapping("/confirm-user")
-	@SendToUser
-	@Auth(role="ROLE_ADMIN")
-	public ApiResponse confirmUser(@Shib Object shibObj, @ReqId String requestId) throws Exception {
-
-		Credentials shib = (Credentials) shibObj;
 		
-		Map<String, Object> userMap = new HashMap<String, Object>();
-		userMap.put("changedUserUin", shib.getUin());
-		
-		if(userRepo.getUserByUin(Long.parseLong(shib.getUin())) == null) {
-    		
-    		AppUser newUser = new AppUser();
-    		
-			newUser.setUin(Long.parseLong(shib.getUin()));					
-			newUser.setRole(shib.getRole());
-			
-			newUser.setFirstName(shib.getFirstName());
-    		newUser.setLastName(shib.getLastName());
-			
-			userRepo.save(newUser);
-			
-			userMap.put("list", userRepo.findAll());
-			
-			this.simpMessagingTemplate.convertAndSend("/channel/users", new ApiResponse("success", userMap, new RequestId(requestId)));
-			
-			return new ApiResponse("success", userMap, new RequestId(requestId));
-		}
-		
-		userMap.put("list", userRepo.findAll());
-				
-		return new ApiResponse("success", userMap, new RequestId(requestId));
-	}
-	
 }
