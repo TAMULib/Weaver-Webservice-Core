6e6f98a2ac65079077027386d86e2e8b8fee0929
William Welling <wwelling@library.tamu.edu>
2016-01-05 07:43:08 -0600

    Improved websocket event logging. Added some additional configuration to the websocket broker and transport.

--- framework/core/src/main/java/edu/tamu/framework/config/CoreWebSocketConfig.java
+++ framework/core/src/main/java/edu/tamu/framework/config/CoreWebSocketConfig.java
@@ -9,11 +9,18 @@
  */
 package edu.tamu.framework.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 /** 
  * Web Socket Configuration.
@@ -22,12 +29,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  *
  */
 @Configuration
+@EnableScheduling
 @EnableWebSocketMessageBroker
-public abstract class CoreWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public abstract class CoreWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer implements SchedulingConfigurer {
+	
+	 @Bean
+	 public ThreadPoolTaskScheduler reservationPool() {
+		 return new ThreadPoolTaskScheduler();
+	 }
 	
 	/**
 	 * Configure message broker. Enables simple broker queue and channel. 
-	 * Sets prefix ws and user destination prefix privat.
+	 * Sets prefix ws and user destination prefix private
 	 * 
 	 * @param       registry    	MessageBrokerRegistry
 	 * 
@@ -49,6 +62,33 @@ public abstract class CoreWebSocketConfig extends AbstractWebSocketMessageBroker
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/connect").setAllowedOrigins("*").withSockJS();
 	}
+	
+	/**
+	 * Configure websocket transport registration.
+	 * 
+	 * @param       registration    	WebSocketTransportRegistration
+	 * 
+	 */
+	@Override
+	public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+		registration.setSendBufferSizeLimit(2 * 512 * 1024);
+		registration.setSendTimeLimit(2 * 10 * 10000);
+	}
+	
+	@Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.taskExecutor().corePoolSize(8).maxPoolSize(Integer.MAX_VALUE);
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(8).maxPoolSize(Integer.MAX_VALUE);
+    }
+    
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setTaskScheduler(reservationPool());
+    }
 	
 }
 
--- framework/core/src/main/java/edu/tamu/framework/service/StompConnectionService.java
+++ framework/core/src/main/java/edu/tamu/framework/service/StompConnectionService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StompConnectionService {
-
+	
 	private static int totalActiveConnections = 0;
 	
 	public synchronized void incrementActiveConnections() {
