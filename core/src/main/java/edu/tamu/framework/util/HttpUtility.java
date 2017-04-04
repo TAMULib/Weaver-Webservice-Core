42a119352bfbf2be0bc3de861cae7c2a335504dd
William Welling <wwelling@library.tamu.edu>
2017-04-04 13:39:04 -0500

    Allow specifying content type in http request.

--- core/src/main/java/edu/tamu/framework/util/HttpUtility.java
+++ core/src/main/java/edu/tamu/framework/util/HttpUtility.java
@@ -37,18 +37,26 @@ public class HttpUtility {
     private int DEFAULT_TIMEOUT;
 
     public String makeHttpRequest(String urlString, String method) throws IOException {
-        return makeHttpRequest(urlString, method, Optional.empty(), DEFAULT_TIMEOUT);
+        return makeHttpRequest(urlString, method, Optional.empty(), Optional.empty(), DEFAULT_TIMEOUT);
     }
 
     public String makeHttpRequest(String urlString, String method, String message) throws IOException {
-        return makeHttpRequest(urlString, method, Optional.of(message), DEFAULT_TIMEOUT);
+        return makeHttpRequest(urlString, method, Optional.of(message), Optional.empty(), DEFAULT_TIMEOUT);
+    }
+
+    public String makeHttpRequest(String urlString, String method, String message, String contentType) throws IOException {
+        return makeHttpRequest(urlString, method, Optional.of(message), Optional.of(contentType), DEFAULT_TIMEOUT);
     }
 
     public String makeHttpRequest(String urlString, String method, String message, int timeout) throws IOException {
-        return makeHttpRequest(urlString, method, Optional.of(message), timeout);
+        return makeHttpRequest(urlString, method, Optional.of(message), Optional.empty(), timeout);
     }
 
-    public String makeHttpRequest(String urlString, String method, Optional<String> message, int timeout) throws IOException {
+    public String makeHttpRequest(String urlString, String method, String message, String contentType, int timeout) throws IOException {
+        return makeHttpRequest(urlString, method, Optional.of(message), Optional.of(contentType), timeout);
+    }
+
+    public String makeHttpRequest(String urlString, String method, Optional<String> message, Optional<String> contentType, int timeout) throws IOException {
         URL url = new URL(urlString);
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -61,7 +69,13 @@ public class HttpUtility {
         conn.setDoOutput(true);
 
         if (message.isPresent()) {
+
             conn.setDoInput(true);
+
+            if (contentType.isPresent()) {
+                conn.setRequestProperty("Content-type", contentType.get());
+            }
+
             PrintWriter pw = new PrintWriter(conn.getOutputStream());
             pw.write(message.get());
             pw.close();
