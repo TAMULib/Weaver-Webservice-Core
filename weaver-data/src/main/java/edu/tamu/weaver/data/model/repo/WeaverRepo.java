27e1496cf0e266414b761d493bfd7f307fd711bb
jeremythuff <jeremythuff@gmail.com>
2017-10-10 11:29:30 -0500

    Added initial weaver data classes

---
+++ weaver-data/src/main/java/edu/tamu/weaver/data/model/repo/WeaverRepo.java
@@ -0,0 +1,16 @@
+package edu.tamu.weaver.data.model.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import edu.tamu.weaver.data.model.WeaverEntity;
+
+@NoRepositoryBean
+public interface WeaverRepo<M extends WeaverEntity> extends JpaRepository<M, Long>, WeaverRepoCustom<M> {
+	
+	@Override
+	public void delete(M model);
+
+}
+
+
