b578ae868869f14750b6ce11dba29ff7c8b462bd
William Welling <wwelling@library.tamu.edu>
2017-10-23 09:05:57 -0500

    return 401 for unauthorized

--- weaver-auth/src/main/java/edu/tamu/weaver/auth/controller/handler/AuthRestExceptionHandler.java
+++ weaver-auth/src/main/java/edu/tamu/weaver/auth/controller/handler/AuthRestExceptionHandler.java
@@ -2,9 +2,12 @@ package edu.tamu.weaver.auth.controller.handler;
 
 import static edu.tamu.weaver.auth.AuthConstants.UNAUTHORIZED_API_RESPONSE;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.tamu.weaver.response.ApiResponse;
@@ -14,6 +17,8 @@ import edu.tamu.weaver.response.ApiResponse;
 public class AuthRestExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    @ResponseBody
     public ApiResponse handleAccessDeniedExceptionRest(AccessDeniedException exception) {
         return UNAUTHORIZED_API_RESPONSE;
     }
