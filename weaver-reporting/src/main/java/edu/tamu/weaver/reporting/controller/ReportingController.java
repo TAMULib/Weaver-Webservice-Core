34ab91c41f252b2df77c9329297f8cc46bd42e27
William Welling <wwelling@library.tamu.edu>
2017-11-29 09:03:13 -0600

    reporting controller depends on email sender

--- weaver-reporting/src/main/java/edu/tamu/weaver/reporting/controller/ReportingController.java
+++ weaver-reporting/src/main/java/edu/tamu/weaver/reporting/controller/ReportingController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ import edu.tamu.weaver.response.ApiResponse;
  *
  */
 @RestController
+@DependsOn("emailSender")
 @RequestMapping("/report")
 public class ReportingController {
 
