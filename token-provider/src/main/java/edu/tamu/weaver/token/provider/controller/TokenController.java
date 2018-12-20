f16d7ce62e33ea98682b6862365740ced5a14411
William Welling <wwelling@library.tamu.edu>
2018-12-20 12:14:32 -0600

    use URIBuilder to preserve query params during redirect with addition of jwt

--- token-provider/src/main/java/edu/tamu/weaver/token/provider/controller/TokenController.java
+++ token-provider/src/main/java/edu/tamu/weaver/token/provider/controller/TokenController.java
@@ -2,6 +2,7 @@ package edu.tamu.weaver.token.provider.controller;
 
 import static edu.tamu.weaver.response.ApiStatus.SUCCESS;
 
+import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
@@ -10,6 +11,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,16 +34,20 @@ public class TokenController {
     protected TokenService tokenService;
 
     @RequestMapping("/token")
-    public RedirectView token(@RequestParam Map<String, String> params, @RequestHeader Map<String, String> headers) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+    public RedirectView token(@RequestParam Map<String, String> params, @RequestHeader Map<String, String> headers) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, URISyntaxException {
         LOG.debug("params: " + params);
         String referer = params.get("referer");
         if (referer == null) {
-            LOG.debug("No referer in params!!");
+            LOG.error("No referer in params!!");
             throw new RuntimeException("No referer in params!!");
         }
+        URIBuilder builder = new URIBuilder(referer);
+        builder.addParameter("jwt", tokenService.craftToken(headers));
+        String url = builder.build().toASCIIString();
+        LOG.debug(String.format("Auth url redirect: %s", url));
         RedirectView redirect = new RedirectView();
         redirect.setContextRelative(false);
-        redirect.setUrl(referer + "?jwt=" + tokenService.craftToken(headers));
+        redirect.setUrl(url);
         return redirect;
     }
 
