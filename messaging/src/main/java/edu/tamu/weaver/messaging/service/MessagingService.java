c10d10cd085a094e86f44acfa0aa63eadec2db07
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-04-22 08:40:36 -0500

    Changed JsonNode to map

--- messaging/src/main/java/edu/tamu/weaver/messaging/service/MessagingService.java
+++ messaging/src/main/java/edu/tamu/weaver/messaging/service/MessagingService.java
@@ -1,12 +1,12 @@
 package edu.tamu.weaver.messaging.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.JsonNode;
 
 import edu.tamu.weaver.messaging.model.MessageActions;
 
@@ -20,8 +20,8 @@ public class MessagingService {
         this.jmsTemplate = jmsTemplate;
     }
 
-    public void sendMessage(String destination, JsonNode payload, MessageActions action) {
-        Message<JsonNode> message = MessageBuilder.withPayload(payload).setHeader("action", action).build();
+    public void sendMessage(String destination, Map<String, String> payload, MessageActions action) {
+        Message<Map<String, String>> message = MessageBuilder.withPayload(payload).setHeader("action", action).build();
         this.jmsTemplate.send(destination, message);
     }
 
