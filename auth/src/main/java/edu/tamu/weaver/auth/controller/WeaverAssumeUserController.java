5f76654ed2099a762917205e7bb8dcd431160b49
William Welling <wwelling@library.tamu.edu>
2020-10-21 12:43:30 -0500

    refactor admin to assume controller and config url

--- auth/src/main/java/edu/tamu/weaver/auth/controller/WeaverAssumeUserController.java
+++ auth/src/main/java/edu/tamu/weaver/auth/controller/WeaverAssumeUserController.java
@@ -1,13 +1,7 @@
 package edu.tamu.weaver.auth.controller;
 
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -19,9 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import edu.tamu.weaver.response.ApiResponse;
 import edu.tamu.weaver.response.ApiStatus;
@@ -31,16 +22,18 @@ import edu.tamu.weaver.utility.HttpUtility;
 /**
  * Opt-in by default.
  *
- * To disable:
- * app.admin.controller.enabled: false
+ * To disable: app.assume.enabled: false
  */
 @RestController
-@RequestMapping("/admin")
-@ConditionalOnProperty(prefix = "app", name = "admin.controller.enabled", havingValue = "true", matchIfMissing = true)
-public class WeaverAdminController {
+@RequestMapping("/assume")
+@ConditionalOnProperty(prefix = "app", name = "assume.enabled", havingValue = "true", matchIfMissing = false)
+public class WeaverAssumeUserController {
 
     @Value("${app.authority.admins}")
     private String[] admins;
+
+    @Value("${app.assume.claims-url}")
+    private String assumeClaimsUrl;
 
     @Autowired
     private TokenService tokenService;
@@ -60,15 +53,16 @@ public class WeaverAdminController {
     @GetMapping
     @SuppressWarnings("unchecked")
     @PreAuthorize("hasRole('ADMIN')")
-    public ApiResponse admin(@RequestParam() Map<String, String> params) throws Exception {
+    public ApiResponse assume(@RequestParam() Map<String, String> params) throws Exception {
 
         ApiResponse apiResponse;
 
         String netid = params.get("netid");
 
-        String response = HttpUtility.makeHttpRequest("http://php.library.tamu.edu/utilities/get_person_info.php?netid=" + netid, "GET");
+        String response = HttpUtility.makeHttpRequest(String.format("%s?netid=%s", assumeClaimsUrl, netid), "GET");
 
         Map<String, String> info = objectMapper.readValue(response, Map.class);
+        info.put("netid", netid);
 
         if (info.get("result") == null) {
 
@@ -91,58 +85,14 @@ public class WeaverAdminController {
                 claims.put("tdl-metadata-edupersonaffiliation", affiliation);
             }
 
-            apiResponse = new ApiResponse(ApiStatus.SUCCESS, "Assume token request successful.", tokenService.craftToken(claims));
+            apiResponse = new ApiResponse(ApiStatus.SUCCESS, "Assume token request successful.",
+                    tokenService.craftToken(claims));
 
         } else {
             apiResponse = new ApiResponse(ApiStatus.INVALID, "netid not found");
         }
 
         return apiResponse;
-    }
-
-    /**
-     * Get UIN from a netid.
-     * 
-     * @param netid String
-     * 
-     * @return String
-     * @throws IOException
-     * @throws SAXException
-     * @throws ParserConfigurationException
-     * 
-     * @throws Exception
-     */
-    protected String getUin(final String netid) throws IOException, ParserConfigurationException, SAXException {
-
-        String response = HttpUtility.makeHttpRequest("http://php.library.tamu.edu/utilities/find_netid_uin.php?netid=" + netid, "GET");
-
-        Document resDoc = convertStringToDocument(response);
-
-        if (resDoc.getElementById("result") != null) {
-            throw new RuntimeException("Please Register IP");
-        }
-
-        response = response.substring(1, response.length() - 1);
-        response = response.split(",")[1];
-        response = response.split(":")[1];
-
-        return response;
-    }
-
-    /**
-     * Convert string to document.
-     * 
-     * @param xmlStr
-     * @return
-     * @throws ParserConfigurationException
-     * @throws IOException
-     * @throws SAXException
-     */
-    private static Document convertStringToDocument(String xmlStr) throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new InputSource(new StringReader(xmlStr)));
-        return doc;
     }
 
 }
